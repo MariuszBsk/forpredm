de2c6e4996adc09fcc20bdb94e1ef0da9b57a1d5
Mariusz <aroundus281@gmail.com>
2025-02-10 18:18:18 +0000

    app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def predict_prices():
         return jsonify({'error': 'Failed to fetch data for prediction'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
 
 
 
